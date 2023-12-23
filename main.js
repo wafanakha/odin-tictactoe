fd2ddb8fbe2079de112813c8faeb0f26db77b839
wafanakha <wafanakha15@gmail.com>
2023-12-23 11:35:57 +0700

    adding game rule to the player score

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const gameRule = function (player, com) {
+  const set = [player, com];
   const rule = () => {
-    console.log();
+    set.forEach((e) => {
+      let ex = e[0];
+      console.log([ex, ex++, ex++]);
+      switch (ex) {
+        case e == [ex, ex++, ex++]:
+          console.log("yuuuhuuu");
+          break;
+
+        default:
+          console.log("gagal");
+          break;
+      }
+    });
   };
 
   return { rule };
 };
 
-const test = gameRule();
-test.halo();
+const test = gameRule([1, 2, 3], [4, 5, 7]);
+test.rule();
