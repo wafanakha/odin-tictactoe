28e632ca31c96088a4ed2a01081b1a2b3e2731d0
wafanakha <wafanakha15@gmail.com>
2023-12-23 11:42:49 +0700

    starting rule over

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 const gameRule = function (player, com) {
   const set = [player, com];
-  const rule = () => {
-    set.forEach((e) => {
-      let ex = e[0];
-      console.log([ex, ex++, ex++]);
-      switch (ex) {
-        case e == [ex, ex++, ex++]:
-          console.log("yuuuhuuu");
-          break;
-
-        default:
-          console.log("gagal");
-          break;
-      }
-    });
-  };
+  const rule = () => {};
 
   return { rule };
 };
