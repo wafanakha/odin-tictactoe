40655e495bb3da7940b7212c998bd0ef3ffdb598
wafanakha <wafanakha15@gmail.com>
2023-12-23 20:53:58 +0700

    acctyally fix combination not compatible with player input

--- main.js
+++ main.js
@@ -10,15 +10,19 @@ const gameRule = function (player, com) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  const combo = combination.join(",");
+  const playercomb = player.join(",");
+  const combo = combination.map((e) => e.join(","));
   const rule = () => {
-    if (combination.some(() => player.join(","))) {
+    console.log(combo);
+    if (combo.some((e) => e == playercomb)) {
       console.log("Youwwwinn");
     }
+    console.log(combo[0]);
+    console.log(playercomb);
   };
 
   return { rule };
 };
 
-const test = gameRule([1, 2, 3], [4, 5, 7]);
+const test = gameRule([0, 1, 2], [4, 5, 7]);
 test.rule();
