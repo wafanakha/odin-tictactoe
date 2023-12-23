d72ed004050d2d75e80af9c3e71dfd0133db6818
wafanakha <wafanakha15@gmail.com>
2023-12-23 21:29:32 +0700

    adding player input

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const gameRule = function (player, com) {
-  const set = [player, com];
+const gameRule = function () {
+  const { player, com } = gameSet();
+  console.log(player, com);
   const combination = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,16 +14,26 @@ const gameRule = function (player, com) {
   const playercomb = player.join(",");
   const combo = combination.map((e) => e.join(","));
   const rule = () => {
-    console.log(combo);
     if (combo.some((e) => e == playercomb)) {
       console.log("Youwwwinn");
     }
-    console.log(combo[0]);
-    console.log(playercomb);
   };
 
   return { rule };
 };
 
-const test = gameRule([0, 1, 2], [4, 5, 7]);
+const gameSet = function () {
+  const player = [];
+  const com = [];
+  const start = () => {
+    player.push(prompt("player"));
+    com.push(Math.floor(Math.random() * 9));
+    com.push(Math.floor(Math.random() * 9));
+    com.push(Math.floor(Math.random() * 9));
+  };
+  start();
+  return { player, com, start };
+};
+
+const test = gameRule();
 test.rule();
